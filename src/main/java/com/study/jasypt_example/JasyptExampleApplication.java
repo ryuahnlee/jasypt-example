bac458d46a1417a85a9b3744955a53638cf1fbb0
ahnlee <ahnlee@danal.co.kr>
2025-05-30 15:47:09 +0900

    chore: jasypt-example 프로젝트 초기 설정

---
+++ src/main/java/com/study/jasypt_example/JasyptExampleApplication.java
@@ -0,0 +1,13 @@
+package com.study.jasypt_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasyptExampleApplication.class, args);
+	}
+
+}
